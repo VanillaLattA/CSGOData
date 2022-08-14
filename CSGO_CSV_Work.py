9a9731069fd970be74aff96076be1e881c2b6b62
Bhargo <77913519+VanillaLattA@users.noreply.github.com>
2022-08-13 19:23:25 -0700

    Closes file and fixes some CSV issues

--- CSGO_CSV_Work.py
+++ CSGO_CSV_Work.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 from csv import DictReader
-
-
 # Python program to read CSV file line by line
 # import necessary packages
 import csv
-    
-# Open file 
-with open('CSGOskinsCSV.csv', 'r') as read_obj:   
-    csv_dict_reader = DictReader(read_obj)
-    #column_names = csv_dict_reader.fieldnames      This reads all the column names
-    
-    def printTypeAndSkin():
+#column_names = csv_dict_reader.fieldnames      This reads all the header names
+
+
+
+# Open file    
+def printTypeAndSkin():
+    with open('CSGOskinsCSV.csv', 'r') as read_obj: #add these two lines whenever iterating 
+        csv_dict_reader = DictReader(read_obj)
         for row in csv_dict_reader:
             print(row['Type'],row['Skin'])
-    printTypeAndSkin()
+    read_obj.close()
+printTypeAndSkin()
 
 with open('CSGOskinsCSV.csv', 'r') as read_obj:   
     csv_dict_reader = DictReader(read_obj)
@@ -31,5 +31,6 @@ with open('CSGOskinsCSV.csv', 'r') as read_obj:
             else:
                 battlescarredSkinPrice += float(row['BattleScarred'])
                 columnLength += 1
-        print('Average battle-scarred price is: ' + str(battlescarredSkinPrice/columnLength))
+        print('Average battle-scarred price is: ' + str(battlescarredSkinPrice/columnLength)) 
     averageBSPrice()
+read_obj.close()
